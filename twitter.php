0e3f71b6fee325cd9131084bc362ed8f02899024
Emilien Schneider <emilien.schneider@gmail.com>
2016-09-16 19:16:11 +0200

    Hop

--- twitter.php
+++ twitter.php
@@ -18,7 +18,12 @@ $influent_accounts_politics = [
 	"fhollande",
 	"manuelvalls",
 	"SLeFoll",
-	"edwyplenel"
+	"BCazeneuve",
+	"SLeFoll",
+	"najatvb",
+	"ChTaubira",
+	"JY_LeDrian",
+	"MarisolTouraine",
 ];
 
 $influent_accounts_news = [
@@ -41,6 +46,29 @@ $influent_accounts_news = [
 	"NathalieSchuck",
 	"EGuedel",
 	"Fanny_Agostini",
+	"edwyplenel"
+];
+
+$influent_shows = [
+	"JJBourdin_RMC",
+	"franceinter",
+	"Europe1",
+	"RTLFrance",
+	"BFMPolitique",
+	"itele",
+	"Qofficiel",
+];
+
+$influent_newspaper = [
+	"libe",
+	"lemondefr",
+	"Le_Figaro",
+	"humanite_fr",
+	"MarianneleMag",
+	"lerepu",
+	"presseocean",
+	"20Minutes",
+	"le_parisien",
 ];
 
 $controversy_subjects = [
@@ -52,6 +80,18 @@ $controversy_subjects = [
 		)
 	),
 	array(
+		"label" => "de la Corée du Nord",
+		"hashtags" => array(
+			"northkorea"
+		)
+	),
+	array(
+		"label" => "du nucléaire iranien",
+		"hashtags" => array(
+			"iran"
+		)
+	),
+	array(
 		"label" => "des Panama papers",
 		"hashtags" => array(
 			"panamapapers",
@@ -59,6 +99,9 @@ $controversy_subjects = [
 			"fraude"
 		)
 	),
+];
+
+$main_subjects = [
 	array(
 		"label" => "du Brexit",
 		"hashtags" => array(
@@ -82,9 +125,53 @@ $controversy_subjects = [
 		)
 	),
 	array(
-		"label" => "de la Corée du Nord",
+		"label" => "de la finance",
 		"hashtags" => array(
-			"northkorea"
+			"finance",
+			"transparence",
+		)
+	),
+	array(
+		"label" => "de la Syrie",
+		"hashtags" => array(
+			"syrie",
+		)
+	),
+	array(
+		"label" => "du numérique",
+		"hashtags" => array(
+			"digital",
+			"numerique",
+			"disruption",
+		)
+	),
+	array(
+		"label" => "de l'Europe",
+		"hashtags" => array(
+			"brexit",
+			"europe",
+			"ue",
+		)
+	),
+	array(
+		"label" => "du nucléaire",
+		"hashtags" => array(
+			"energie",
+			"nucleaire"
+		)
+	),
+	array(
+		"label" => "du Grand Paris",
+		"hashtags" => array(
+			"paris",
+			"decentralisation",
+		)
+	),
+	array(
+		"label" => "de la sécurité",
+		"hashtags" => array(
+			"securite",
+			"terrorisme",
 		)
 	),
 ];
@@ -101,7 +188,10 @@ $cities = [
 ];
 
 $own_hashtags = [
-	"ppl2017"
+	"",
+	"",
+	"",
+	" #ppl2017"
 ];
 
 
@@ -111,30 +201,65 @@ mt_srand();
 
 
 $punct_list = [
-    ".", ".", " !", "...", ".", "..."
+    ".", ".", ".", ".", ".", " !", " !", "...", "..."
 ];
 
 
 $politic = $influent_accounts_politics[mt_rand(0, count($influent_accounts_politics) -1)];
 $journalist = $influent_accounts_news[mt_rand(0, count($influent_accounts_news) -1)];
 $city = $cities[mt_rand(0, count($cities) -1)];
+$show = $influent_shows[mt_rand(0, count($influent_shows) -1)];
+$newspaper = $influent_newspaper[mt_rand(0, count($influent_newspaper) -1)];
 
-$subject = $controversy_subjects[mt_rand(0, count($controversy_subjects) -1)];
+$own_hashtag = $own_hashtags[mt_rand(0, count($own_hashtags) -1)];
+
+$controversy_subject = $controversy_subjects[mt_rand(0, count($controversy_subjects) -1)];
+$controversy_subject_label = $controversy_subject["label"];
+$controversy_hashtag = $controversy_subject["hashtags"][mt_rand(0, count($controversy_subject["hashtags"]) -1)];
+
+$subject = $main_subjects[mt_rand(0, count($main_subjects) -1)];
 $subject_label = $subject["label"];
 $hashtag = $subject["hashtags"][mt_rand(0, count($subject["hashtags"]) -1)];
 
 $punct = $punct_list[mt_rand(0, count($punct_list) -1)];
 
-//    $e = $buzz_words[mt_rand(0, count($buzz_words) -1)];
-//    $f = $buzz_words[mt_rand(0, count($buzz_words) -1)];
-
 $phrases = [
 	"Belle rencontre avec @$politic au sujet $subject_label$punct #$hashtag",
 	"Les positions de @$politic pour ce qui est $subject_label m'inquiètent$punct #$hashtag",
 	"La dernière déclaration de @$politic à propos $subject_label n'est pas une bonne nouvelle$punct",
-	"On n'avance vraiment pas au sujet $subject_label$punct, n'est-ce-pas @$journalist ?",
+	"Je tiens à saluer le travail de @$politic à propos $subject_label$punct",
+	"Je suis assez surpris des positions de @$politic en faveur $subject_label$punct",
+	"Il serait bon de ne pas bafouer les valeurs de la république, @$politic$punct",
+	"Ce qui intéresse les français, c'est de clarifier les avancées à propos $subject_label, @$politic$punct",
+	"Le gouvernement n'a pas assez fait d'effort en faveur $subject_label, n'est-ce-pas @$politic ?",
+	
+	"On n'avance vraiment pas au sujet $subject_label, n'est-ce-pas @$journalist ?",
 	"Il faudrait que @$journalist aborde un peu plus le thème $subject_label$punct #$hashtag",
-	"Rendez-vous demain à $city avec @$journalist pour parler $subject_label$punct",
+	"Nos concitoyens seraient intéressés pour en savoir plus sur le thème $subject_label, @$journalist$punct #$hashtag",
+	"Rendez-vous demain à $city avec @$journalist pour parler $subject_label$punct$own_hashtag",
+	
+	"J'attends toujours l'invitation de @$show pour pouvoir exposer mon programme aux français$punct$own_hashtag",
+	"Sur @$show, on ne parle toujours pas des vrais sujets, par exemple $subject_label$punct",
+	"Très honoré d'intervenir demain sur @$show pour parler $subject_label$punct$own_hashtag",
+	
+	"Retrouvez mon interview demain dans @$newspaper$punct J'y parlerai $subject_label$punct$own_hashtag",
+	"Quand @$newspaper m'invitera, nous discuterons enfin des vrais sujets$punct Par exemple $subject_label$punct #$hashtag",
+	"Merci à @$newspaper pour avoir repris mes propos avec précision$punct$own_hashtag",
+	
+	"On s'attarde trop au sujet $controversy_subject_label$punct Et si on parlait $subject_label ? #$controversy_hashtag #$hashtag",
+	"Tous ces débats autour $controversy_subject_label ne mènent à rien$punct #$controversy_hashtag",
+	"Les esprits s'échauffent vite quand on aborde la question $controversy_subject_label$punct #$controversy_hashtag",
+	
+	"Cette semaine, je serai à $city à la rencontre des français pour parler $subject_label$punct$own_hashtag",
+	"Vous connaissez mes opinions, je ne vais pas polémiquer au sujet $subject_label$punct #$hashtag$own_hashtag",
+	"Je suis fier d'annoncer un comité de réflexion à propos $subject_label$punct #$hashtag$own_hashtag",
+	"Notre pays mérite qu'on s'engage, que l'on prenne des risques$punct$own_hashtag",
+	"Innovation, recherche, c'est cela qui est nécessaire quand on aborde la question $subject_label$punct #$hashtag",
+	"Pensée à tous les acteurs réunis autour de la question $subject_label$punct Il est temps que les choses bougent$punct #$hashtag",
+	"Restons mobilisés au sujet $subject_label$punct Prenons le temps d'en parler$punct$own_hashtag",
+	"Un objectif : garder le cap sur nos valeurs au sujet $subject_label$punct #$hashtag$own_hashtag",
+	"Là où certains élus refusent de s'engager, je serai intransigeant à propos $subject_label$punct$own_hashtag",
+	
 ];
 
 
